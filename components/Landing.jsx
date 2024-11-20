c50655d9796a2819537d6017ea36274de61bbe3f
Jason-pixel425 <jasonfitzg2798@gmail.com>
2024-11-20 15:23:06 +1000

    Styling and elements done for landing component
    Just need functionality

---
+++ components/Landing.jsx
@@ -0,0 +1,12 @@
+import '../styles/Landing.css'
+
+
+export default function Landing() {
+    return (
+        <section className="Landing--section">
+            <h1>Quizzical</h1>
+            <h2>Test your knowledge with some trivia</h2>
+            <button>Start Quiz</button>
+        </section>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
+import Landing from '../components/Landing'
 import '../styles/App.css'
 
 function App() {
-  
+  const [startQuiz, setStartQuiz] = useState(false)
 
   return (
     <>
-
+      <Landing />
     </>
   )
 }
