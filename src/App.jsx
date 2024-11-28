4baccd0c61869bb9ea366bdfb96116e659ac4750
Jason-pixel425 <jasonfitzg2798@gmail.com>
2024-11-28 19:30:02 +1000

    Setting trivia questions from getting data from trivia api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Landing from '../components/Landing'
 import '../styles/App.css'
 
 function App() {
+  const [triviaQuestions, setTriviaQuestions] = useState([])
 
 
+  function getTriviaQuestions() {
+    try{
+    fetch('https://opentdb.com/api.php?amount=10')
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data)
+      setTriviaQuestions(data.results)
+    })
+    } catch(e){
+      console.log(e)
+    }
+}
 
   return (
     <>
-      {<Landing handleClick={startQuestions} />}
+      {triviaQuestions.length === 0 && <Landing handleClick={getTriviaQuestions}/>}
     </>
   )
 }
